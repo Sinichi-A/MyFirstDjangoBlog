66cd51d8c3cc702947621d8ce6238536aead268d
username <rm0135ri@ed.ritsumei.ac.jp>
2021-03-03 13:53:21 +0900

    Initial Commit

---
+++ posts/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Post
+
+def index(request):
+    # return HttpResponse("Hello World! 当行のインデックスです。")
+    posts = Post.objects.order_by('-published')
+    return render(request, 'posts/index.html', {'posts': posts})
+
+def homedesu(request):
+    return HttpResponse("ここは僕らのHomeなんだから")
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'posts/post_detail.html', {'post':post})
+
+def about(request):
+    return render(request, 'posts/about.html')
+
